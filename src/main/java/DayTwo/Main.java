27bf673a71d3c12a08f234ddc4d09c6dac5bc849
ennaj_janne <ennaj_janne@hotmail.com>
2024-02-02 18:13:55 +0100

    day three, ready to go.

--- src/main/java/DayTwo/Main.java
+++ src/main/java/DayTwo/Main.java
@@ -17,24 +17,5 @@ public class Main {
             throw new RuntimeException(e);
         }
 
-
-//        String testLine = "Game 1: 13 green, 3 red; 4 red, 9 green, 4 blue; 9 green, 10 red, 2 blue";
-//        DayTwoSolver dayTwoSolver = new DayTwoSolver();
-//        String gameName = dayTwoSolver.getGameName(testLine);
-//        String lineWithoutGameName = dayTwoSolver.getGameLineWithoutGameName(testLine);
-//        System.out.println(gameName);
-//        System.out.println(lineWithoutGameName);
-//        int game = dayTwoSolver.getNumberFromString(gameName);
-//        System.out.println(game);
-//        String[] gameHands = dayTwoSolver.getHands(lineWithoutGameName);
-//        System.out.println(gameHands[0] + "\n" + gameHands[1] + "\n" + gameHands[2] + "\n" + gameHands[3] + "\n" + gameHands[4] + "\n" + gameHands[5] + "\n" + gameHands[6] + "\n" + gameHands[7]);
-//        int highestGreen = dayTwoSolver.getHighestNumberFromColor(gameHands,"green");
-//        int highestRed = dayTwoSolver.getHighestNumberFromColor(gameHands,"red");
-//        int highestBlue = dayTwoSolver.getHighestNumberFromColor(gameHands,"blue");
-//        System.out.println("highest green = " + highestGreen + "\n" +
-//                "highest red = " + highestRed + "\n" +
-//                "highest blue = " + highestBlue);
-//        boolean possibleGame = dayTwoSolver.IsGamePossible(highestRed,highestGreen,highestBlue);
-//        System.out.println("is game possible? " + possibleGame);
     }
 }
